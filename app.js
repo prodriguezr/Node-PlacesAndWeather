fdae59d5ba9f77a634e721e9cbbde5a961b005bc
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-30 16:13:28 -0400

    Create menu

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello, World!');
+const { readInput, showMenu, pause } = require("./helpers/inquirer");
+
+const main = async() => {
+    let option = 0;
+
+    console.clear();
+
+    do {
+        option = await showMenu();
+
+        switch (option) {
+            case 1:
+                
+                break;
+        
+            case 2:
+                
+                break;
+        }
+        if (option !== 0) 
+            await pause();
+
+    } while (option !== 0)
+}
+
+main();
